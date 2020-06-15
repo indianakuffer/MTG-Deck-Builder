93b31a2343dede3603e937bf0c7bf396db6d1fa0
Indiana Kuffer <indianakuffer@gmail.com>
2020-06-15 11:58:49 -0400

    code commenting and cleaning

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+const pageSize = 50
 let previousSearch
-let pageSize = 50
 let deck = {
   name: 'defaultDeck',
   contents: {},
@@ -14,7 +14,39 @@ loadDeck()
 
 // Functions
 
+async function fetchCards(page = 1) {
+  // Gets card information, then renders to page.
+  // Creates/removes the loading indicator, hides/shows appropriate pagination buttons, stores the api url
+
+  let url = `https://api.magicthegathering.io/v1/cards?pageSize=${pageSize}&page=${page}`
+  url += buildQueries()
+
+  document.querySelector('#card-container').innerHTML = ''
+
+  let interval = createLoading()
+
+  try {
+    const response = await axios.get(url)
+    const cardList = response.data.cards
+    removeLoading(interval)
+    paginationHiding(cardList, page)
+    if (cardList.length === 0) {
+      alert('Sorry, that returned no results.')
+      return
+    }
+    cardList.forEach((card) => {
+      renderCard(card)
+    })
+  } catch (error) {
+    console.error(error)
+  }
+  previousSearch = url
+}
+
 function buildQueries() {
+  // Checks the form values and returns queries as one string to append to the API get request
+  // Called in fetchCards()
+
   let queryList = []
 
   // name
@@ -49,31 +81,11 @@ function buildQueries() {
   return parsedQueries
 }
 
-async function fetchCards(page = 1) {
-  let url = `https://api.magicthegathering.io/v1/cards?pageSize=${pageSize}&page=${page}`
-  url += buildQueries()
-  // clears card-container on reload
-  document.querySelector('#card-container').innerHTML = ''
-  let interval = createLoading()
-  try {
-    const response = await axios.get(url)
-    const cardList = response.data.cards
-    removeLoading(interval)
-    paginationHiding(cardList, page)
-    if (cardList.length === 0) {
-      alert('Sorry, that returned no results.')
-    }
-    cardList.forEach((card) => {
-      renderCard(card)
-    })
-  } catch (error) {
-    console.error(error)
-  }
-  previousSearch = url
-}
-
 function renderCard(card) {
-  // cards with no imageUrls seem to be duplicates, skips over those
+  // Creates an element for the card and appends to the card-container, skipping over those with no images
+  // Gives each card an event listener for the moreDetails() function
+  // Called in fetchCards() in forEach loop
+
   if (!card.imageUrl) { return }
 
   const cardElement = document.createElement('img')
@@ -85,6 +97,9 @@ function renderCard(card) {
 }
 
 function paginationHiding(cardList, page) {
+  // Hides pagination buttons based on the page of the previous search or the number of cards returned
+  // Called in fetchCards()
+
   const pageBtnContainer = document.querySelector('#page-btn-container')
   const pageBtnLeft = document.querySelector('.page-btn.left')
   const pageBtnRight = document.querySelector('.page-btn.right')
@@ -106,13 +121,18 @@ function paginationHiding(cardList, page) {
 }
 
 function turnPage(dir) {
+  // Gets the current page number from the previousSearch, then searches again based on the direction
+  // Added with eventListener to pagination buttons
+
   if (!previousSearch) {
     alert('Make a search first!')
     return
   }
+
   const pageLocation = previousSearch.indexOf('page=')
   let pageString = previousSearch.substring(pageLocation, pageLocation + 6)
   let pageNum = parseInt(pageString[pageString.length - 1])
+
   if (pageNum + dir === 0) {
     alert('You\'re at the first page.')
     return
@@ -122,6 +142,10 @@ function turnPage(dir) {
 }
 
 function moreDetails(card) {
+  // Creates card information elements and Add to Deck button, appends them to detail-container
+  // Clears innerHTML except for first child (header)
+  // Added with eventListener to rendered cards, adds the card
+
   const detailContainer = document.querySelector('#detail-container')
 
   while (detailContainer.childNodes.length > 2) {
@@ -183,6 +207,9 @@ function moreDetails(card) {
 }
 
 function toggleDeckView() {
+  // Shows/hides detail-container vs deck-container within the detail-panel
+  // Added with eventListener for detail-view-btn, called elsewhere
+
   const detailContainer = document.querySelector('#detail-container')
   const deckContainer = document.querySelector('#deck-container')
   const iconImage = document.querySelector('#detail-view-btn')
@@ -193,6 +220,9 @@ function toggleDeckView() {
 }
 
 function addToDeck(card) {
+  // Adds the card to the deck, if already present increments the quantity
+  // Added with eventListener to Add to Deck button
+
   if (Object.keys(deck.contents).includes(card.name)) {
     deck.contents[card.name].quantity += 1
   } else {
@@ -203,9 +233,16 @@ function addToDeck(card) {
 }
 
 function renderDeckList() {
+  // Renders the deck's cards to the deck list
+  // Appends add and subtract buttons
+  // Saves the deck after every rendering (after every change)
+  // Hides the Test Hand button if deck has no cards
+
   const testHandButton = document.querySelector('#test-hand-btn')
+
   document.querySelector('#deck').innerHTML = ''
   document.querySelector('#deck-length').textContent = deckLength()
+
   for (const card in deck.contents) {
     const listing = document.createElement('div')
     listing.classList.add('listing')
@@ -251,6 +288,9 @@ function renderDeckList() {
 }
 
 function crementCard(card, amount) {
+  // Changes the card's quantity by the amount, removes card if amount is less than 0
+  // Added with eventListener to deck list add and subtract buttons
+
   deck.contents[card].quantity += amount
   if (deck.contents[card].quantity <= 0) {
     delete deck.contents[card]
@@ -259,6 +299,9 @@ function crementCard(card, amount) {
 }
 
 function deckLength() {
+  // Returns the number of cards in the deck (taking into accoutn quantity), not just the length of the object
+  // Called in renderDeckList() to update the header information
+
   let length = 0
   for (const card in deck.contents) {
     length += deck.contents[card].quantity
@@ -267,7 +310,11 @@ function deckLength() {
 }
 
 function shuffleDeck() {
+  // Returns an array of all the cards in the deck, shuffled
+  // Called in testHand()
+
   let shuffledDeck = []
+
   for (const card in deck.contents) {
     let quantity = deck.contents[card].quantity
     while (quantity > 0) {
@@ -275,6 +322,7 @@ function shuffleDeck() {
       quantity--
     }
   }
+
   shuffledDeck.forEach((card, index) => {
     let randomIndex = (Math.floor(Math.random() * shuffledDeck.length))
     let temp = card
@@ -287,24 +335,33 @@ function shuffleDeck() {
 }
 
 function testHand() {
+  // Creates the test hand of 7 random cards from the deck, appends to and unhides test-hand-container
+
   const handContainer = document.querySelector('#test-hand')
-  handContainer.innerHTML = ''
   const shuffledDeck = shuffleDeck()
   let hand = []
+  handContainer.innerHTML = ''
+
   for (let i = 0; i < shuffledDeck.length; i++) {
     if (i > 6) { continue }
     hand.push(shuffledDeck[i])
   }
+
   hand.forEach(card => {
     const image = document.createElement('img')
     image.src = card.imageUrl
     handContainer.append(image)
   })
+
   if (shuffledDeck.length < 7) { alert('Warning: You have less than 7 cards in your deck!') }
   toggleHidden(document.querySelector('#test-hand-container'))
 }
 
 function createLoading() {
+  // Creates loading signifier
+  // Called in fetchCards()
+  // Returns interval to be cleared later
+
   const loading = document.createElement('div')
   loading.id = 'loading'
   loading.classList.add('starter-text')
@@ -317,28 +374,40 @@ function createLoading() {
 }
 
 function loadingDots() {
+  // Loading dot animation, adds dots until limit is reached
+  // Returns interval to be cleared later
+
   const dots = document.querySelector('#loading-dots')
+
   let interval = setInterval(() => {
     dots.textContent.length >= 3 ? dots.textContent = '' : null
     dots.textContent += '.'
   }, 500)
+
   return interval
 }
 
 function removeLoading(interval) {
+  // Removes loading signifier interval
+  // Called in fetchCards()
+
   clearInterval(interval)
   document.querySelector('#loading').remove()
 }
 
 async function fillSets() {
+  // Fetches set list from api and fills dropdown
+
   try {
     const response = await axios.get('https://api.magicthegathering.io/v1/sets')
     const dropdown = document.querySelector('#input-sets')
     dropdown.innerHTML = ''
+
     const defaultOption = document.createElement('option')
     defaultOption.textContent = '--'
     defaultOption.value = ''
     dropdown.append(defaultOption)
+
     response.data.sets.forEach(set => {
       const newOption = document.createElement('option')
       newOption.textContent = set.name
@@ -351,14 +420,18 @@ async function fillSets() {
 }
 
 async function fillTypes() {
+  // Fetches type list from api and fills dropdown
+
   try {
     const response = await axios.get('https://api.magicthegathering.io/v1/types')
     const dropdown = document.querySelector('#input-types')
     dropdown.innerHTML = ''
+
     const defaultOption = document.createElement('option')
     defaultOption.textContent = '--'
     defaultOption.value = ''
     dropdown.append(defaultOption)
+
     response.data.types.forEach(type => {
       const newOption = document.createElement('option')
       newOption.textContent = type
@@ -371,14 +444,18 @@ async function fillTypes() {
 }
 
 function toggleHidden(element) {
+  // Helper function
   element.classList.toggle('hidden')
 }
 
 function initEventListeners() {
+  // Adds starter event listeners
+
   const searchFormContainer = document.querySelector('#search-form-container')
   const testHandContainer = document.querySelector('#test-hand-container')
   const deckListContainer = document.querySelector('#deck-list-container')
 
+  // form group
   document.querySelector('form').addEventListener('submit', () => {
     event.preventDefault()
     fetchCards()
@@ -389,16 +466,24 @@ function initEventListeners() {
   document.querySelector('#search-form').addEventListener('click', () => {
     event.stopPropagation()
   })
+
+  // test hand container group
   testHandContainer.addEventListener('click', () => toggleHidden(testHandContainer))
   document.querySelector('#test-hand').addEventListener('click', () => {
     event.stopPropagation()
   })
+
+  // deck list container group
   deckListContainer.addEventListener('click', () => toggleHidden(deckListContainer))
   document.querySelector('#deck-list').addEventListener('click', () => {
     event.stopPropagation()
   })
+
+  // pagination group
   document.querySelector('.page-btn.left').addEventListener('click', () => turnPage(-1))
   document.querySelector('.page-btn.right').addEventListener('click', () => turnPage(1))
+
+  // button group
   document.querySelector('#detail-view-btn').addEventListener('click', toggleDeckView)
   document.querySelector('#test-hand-btn').addEventListener('click', testHand)
   document.querySelector('#deck-swap-btn').addEventListener('click', () => {
@@ -408,10 +493,14 @@ function initEventListeners() {
 }
 
 function fillDeckList() {
+  // Renders all decks from localStorage to the deck list
+  // Adds remove deck button and functionality
+
   const localKeys = Object.keys(localStorage)
   const deckListContainer = document.querySelector('#deck-list-container')
   const deckList = document.querySelector('#deck-list')
   deckList.innerHTML = ''
+
   const newDeckButton = document.createElement('button')
   newDeckButton.classList.add('btn')
   newDeckButton.textContent = '+Add Deck'
@@ -442,17 +531,25 @@ function fillDeckList() {
 }
 
 function removeDeck(key) {
+  // Deletes the designated deck, loading defaultDeck is the currently loaded deck was deleted
+  // Added with eventListener to remove-deck-btn
+  // Re-renders deck list
+
   delete localStorage[key]
+
   if (deck.name === key) {
     deck.contents = {}
     loadDeck('defaultDeck')
     alert('Deleted currently loaded deck, loading \'defaultDeck\'')
   }
+
   fillDeckList()
   renderDeckList()
 }
 
 function loadDeck(deckName = 'defaultDeck') {
+  // Saves current deck, then loads the specified deck from localStorage
+
   try {
     Object.keys(deck.contents).length > 0 ? saveDeck() : null
     const loadedDeck = JSON.parse(localStorage[deckName])
@@ -468,16 +565,21 @@ function loadDeck(deckName = 'defaultDeck') {
 }
 
 function saveDeck(deckName = deck.name) {
+  // Saves current deck to localStorage
   localStorage.setItem(deckName, JSON.stringify(deck.contents))
 }
 
 function newDeck() {
+  // Saves the current deck, then creates a new one named by user input
+  // Loads new deck automatically, and switches detail panel to deck view
+
   saveDeck()
   deck.name = window.prompt('Enter the new deck\'s name')
   deck.contents = {}
   saveDeck()
   loadDeck(deck.name)
   fillDeckList()
+
   if (document.querySelector('#deck-container').classList.contains('hidden')) {
     toggleDeckView()
   }
